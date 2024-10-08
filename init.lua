cb5ddc78b2054ca0b59756ac5ed63f722e5d462b
Hellagur4225 <Hellagur4225@outlook.com>
2024-10-08 17:58:59 +0800

    Add lsp templates

--- init.lua
+++ init.lua
@@ -1,1 +1,28 @@
-require("lazy").setup("plugins")
+require('lazy').setup({
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+})
+
+local status, mason = pcall(require, "mason")
+if not status then
+	vim.notify("Could not find meson.")
+	return
+end
+
+local status, lspconfig = pcall(require, "lspconfig")
+if not status then
+	vim.notify("Could not find meson.")
+	return
+end
+
+local status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not status then
+	vim.notify("Could not find meson.")
+	return
+end
+
+mason.setup({})
+lspconfig.setup({})
+mason_lspconfig.setup({})
+

5eca31f6cb7c5fda87c34a527f4ac66ef5ac1d21
Hellagur4225 <Hellagur4225@outlook.com>
2024-10-09 10:53:14 +0800

    Add the lua lsp & a load function

--- init.lua
+++ init.lua
@@ -1,28 +1,44 @@
-require('lazy').setup({
+local plugin_list = {
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'neovim/nvim-lspconfig',
-})
+}
+-- local module_list = {"mason","lspconfig","mason-lspconfig"}
+local module_list = {"mason", "mason-lspconfig"}
 
-local status, mason = pcall(require, "mason")
-if not status then
-	vim.notify("Could not find meson.")
-	return
+require('lazy').setup(plugin_list)
+
+function Load_and_setup_modules(list)
+    for _, mname in ipairs(list) do
+        local status, instance = pcall(require, mname)
+        if not status then
+            vim.notify("Could not find module: " .. mname .. "\nError: " .. instance, vim.log.levels.ERROR)
+            return nil
+        end
+
+        instance.setup({}) -- TODO: Add setup() parameters if needed.
+    end
 end
 
-local status, lspconfig = pcall(require, "lspconfig")
-if not status then
-	vim.notify("Could not find meson.")
-	return
-end
+Load_and_setup_modules(module_list)
 
-local status, mason_lspconfig = pcall(require, "mason-lspconfig")
-if not status then
-	vim.notify("Could not find meson.")
-	return
-end
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
+require("lspconfig").lua_ls.setup { -- TODO: Install some lsp via lua script
+  on_init = function(client)
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'LuaJIT'
+      },
 
-mason.setup({})
-lspconfig.setup({})
-mason_lspconfig.setup({})
-
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+        }
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+}

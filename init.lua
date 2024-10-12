bc603fe7dc722dc950a486d0f54d784583aea382
Hellagur4225 <Hellagur4225@outlook.com>
2024-10-12 17:56:57 +0800

    Add the basic key binding

--- init.lua
+++ init.lua
@@ -4,6 +4,30 @@ local plugin_list = {
     'neovim/nvim-lspconfig',
 }
 
+-- Key binding setup
+vim.wo.relativenumber = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local map = vim.api.nvim_set_keymap
+local opt = {noremap = true, silent = true }
+-- Indent code in visual mode
+map("v", "<", "<gv", opt)
+map("v", ">", ">gv", opt)
+-- Move the selected text up or down
+map("v", "J", ":move '>+1<CR>gv-gv", opt)
+map("v", "K", ":move '<-2<CR>gv-gv", opt)
+-- Move up and down four lines
+map("n", "<C-j>", "4j", opt)
+map("n", "<C-k>", "4k", opt)
+map("n", "<C-s>", ":w<CR>", opt)
+map("i", "<C-s>", "<Esc>:w<CR>a", opt)
+-- Key binding end
+
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
 local function lspconfig_setup(module)
     module.lua_ls.setup { -- TODO: Install some lsp via lua script

1a0d69c01ead02831735489a9d7956c5150ca41b
Hellagur4225 <Hellagur4225@outlook.com>
2024-10-16 14:53:28 +0800

    Add simple completion support

--- init.lua
+++ init.lua
@@ -1,14 +1,29 @@
 local lib = require("lib")
 require("key_binding")
 
+local nvim_cmp = {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-path",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets"
+    }
+}
+
 local plugin_list = {
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'neovim/nvim-lspconfig',
-    --'hrsh7th/nvim-cmp',
-    --'lukas-reineke/indent-blankline.nvim',
+    nvim_cmp,
     'nvim-tree/nvim-tree.lua',
     'akinsho/bufferline.nvim',
+    --'lukas-reineke/indent-blankline.nvim', -- TODO: Accomplish them
+    --'nvim-treesitter/nvim-treesitter'
+    --'windwp/nvim-autopairs',
+    --'nvim-telescope/telescope.nvim'
+    --'lewis6991/gitsigns.nvim'
 }
 
 local module_table = {
@@ -17,6 +32,7 @@ local module_table = {
     ["mason-lspconfig"] = false,
     ["nvim-tree"] = false,
     bufferline = false,
+    cmp = lib.cmp_setup,
 }
 
 if lib.check_lazynvim(plugin_list) then
--- lua/lib.lua
+++ lua/lib.lua
@@ -1,30 +1,5 @@
 local lib = {}
 
-
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
-lib.lspconfig_setup = function(module)
-    module.lua_ls.setup { -- TODO: Install some lsp via lua script
-        on_init = function(client)
-            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                runtime = {
-                    version = 'LuaJIT'
-                },
-
-                workspace = {
-                    checkThirdParty = false,
-                    library = {
-                        vim.env.VIMRUNTIME,
-                        "${3rd}/luv/library" -- Related to vim.uv
-                    }
-                }
-            })
-        end,
-        settings = {
-            Lua = {}
-        }
-    }
-end
-
 -- https://www.lazyvim.org/configuration/lazy.nvim
 local function download_lazynvim(lazypath)
     print("Download lazynvim from github...")
@@ -50,7 +25,8 @@ function lib.check_lazynvim(plugin_list)
         lazy.setup(plugin_list)
         return true
     else
-        return download_lazynvim(lazypath) end
+        return download_lazynvim(lazypath)
+    end
 end
 
 function lib.load_and_setup_modules(list)
@@ -67,6 +43,56 @@ function lib.load_and_setup_modules(list)
             callback(instance)
         end
     end
+end
+
+-- TODO: Move to setup.lua
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
+lib.lspconfig_setup = function(module)
+    module.lua_ls.setup({ -- TODO: Install some lsp via lua script
+        on_init = function(client)
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                runtime = {
+                    version = 'LuaJIT'
+                },
+
+                workspace = {
+                    checkThirdParty = false,
+                    library = {
+                        vim.env.VIMRUNTIME,
+                        "${3rd}/luv/library" -- Related to vim.uv
+                    }
+                }
+            })
+        end,
+        settings = {
+            Lua = {}
+        }
+    })
+end
+
+-- https://github.com/hrsh7th/nvim-cmp/blob/main/README.md#setup
+lib.cmp_setup = function(module)
+    local temp_setting = {
+        snippet = {
+            expand = function(args)
+                vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            end,
+        },
+        sources = module.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'vsnip' }, -- For vsnip users.
+            { name = 'luasnip' }, -- For luasnip users.
+            -- { name = 'ultisnips' }, -- For ultisnips users.
+            -- { name = 'snippy' }, -- For snippy users.
+        }, {
+            { name = 'buffer' },
+        })
+    }
+
+    module.setup(temp_setting)
 end
 
 return lib

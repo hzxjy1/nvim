26bac3a42bf3342837cdbfbd4078f72832f23f4a
archetto <archetto@Landen.hzxjy.art>
2024-11-14 11:04:23 +0800

    Disable rainbow

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -22,12 +22,12 @@ local setup = {
         enable = true,
         additional_vim_regex_highlighting = false
     },
-    rainbow = { enable = true, extended_mode = true, max_file_lines = nil }
+    -- rainbow = { enable = true, extended_mode = true, max_file_lines = nil }
 }
 
 local config = {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = { "p00f/nvim-ts-rainbow" },
+    -- dependencies = { "p00f/nvim-ts-rainbow" }, -- Have a unknown error Vim:E475 in debian12
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup(setup)

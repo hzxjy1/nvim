fc7c47f7daf32470b353b297e7aeb5055e748896
Hellagur4225 <Hellagur4225@outlook.com>
2024-11-15 16:52:40 +0800

    Use grobal values lib & fp

--- init.lua
+++ init.lua
@@ -7,24 +7,31 @@
 -- TODO: Add ui switch key_binding
 -- TODO: fmt while enter normal mode
 -- TODO: Add cmd to reload conf manually
-local lib = require("lib")
+
+---@diagnostic disable: lowercase-global
+-- Lowercase for compatibility -- TODO: Del unuseful import
+lib = require("lib")
+fp = require("functional")
+
 require("key_binding")
 local wish_list = {
-    --'lukas-reineke/indent-blankline.nvim', -- TODO: Accomplish them
-    --'nvim-telescope/telescope.nvim'
-    --'lewis6991/gitsigns.nvim'
-    --'stevearc/aerial.nvim'
-    --'CRAG666/code_runner.nvim'
-    --'nvimdev/lspsaga.nvim'
-    --"folke/which-key.nvim"
-    --'tpope/vim-obsession'
-    --'rmagatti/auto-session'
-    --"karb94/neoscroll.nvim"
-    --"azabiong/vim-highlighter"
+	--'lukas-reineke/indent-blankline.nvim', -- TODO: Accomplish them
+	--'nvim-telescope/telescope.nvim'
+	--'lewis6991/gitsigns.nvim'
+	--'stevearc/aerial.nvim'
+	--'CRAG666/code_runner.nvim'
+	--'nvimdev/lspsaga.nvim'
+	--"folke/which-key.nvim"
+	--'tpope/vim-obsession'
+	--'rmagatti/auto-session'
+	--"karb94/neoscroll.nvim"
+	--"azabiong/vim-highlighter"
 }
+
+lib.check_essential(require("conf").essential_bin)
 
 local plugin_path = "plugins"
 local plugin_list = lib.module_loader(plugin_path)
 if not lib.lazynvim_bootstrap(plugin_list) then
-    print("Unable to load plugins")
+	print("Unable to load plugins")
 end

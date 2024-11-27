d8875c713ea6fceae1ef333d17197cc986cc38c1
Hellagur4225 <Hellagur4225@outlook.com>
2024-11-27 18:31:04 +0800

    Add json formatter & fix a nil bug

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,7 +4,9 @@ local config = {
 		local lang_conf = lib.module_loader("trinity") -- TODO: Param loaded by conf.lua
 		local lspconfig = require("lspconfig")
 		fp.map(lang_conf, function(obj)
-			lspconfig[obj.lsp].setup(obj.lsp_setup(lspconfig))
+			if obj.lsp ~= nil then
+				lspconfig[obj.lsp].setup(obj.lsp_setup(lspconfig))
+			end
 		end)
 	end,
 }

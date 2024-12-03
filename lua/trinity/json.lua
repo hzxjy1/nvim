6516498cfbc50f5fd7406db0b52bfffa175087f5
Hellagur4225 <Hellagur4225@outlook.com>
2024-12-03 09:25:38 +0800

    Enhance json support

--- lua/trinity/json.lua
+++ lua/trinity/json.lua
@@ -1,8 +1,21 @@
 local json_conf = {
 	name = "json",
-	lsp = nil,
+	lsp = "jsonls",
 	linter = nil,
-	formatter = "prettier",
+	formatter = "biome",
 }
+
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#jsonls
+json_conf.lsp_setup = function(lspconfig)
+	return {
+		cmd = { "vscode-json-language-server", "--stdio" },
+		filetypes = { "json", "jsonc" },
+		init_options = {
+			provideFormatter = false,
+		},
+		root_dir = lspconfig.util.find_git_ancestor,
+		single_file_support = true,
+	}
+end
 
 return json_conf

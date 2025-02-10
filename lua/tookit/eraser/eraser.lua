4f32d58fd1bada4209f98fc15526ba612a42b66b
Hellagur4225 <Hellagur4225@outlook.com>
2025-02-10 13:34:13 +0800

    Locate commits

--- lua/tookit/eraser/eraser.lua
+++ lua/tookit/eraser/eraser.lua
@@ -11,45 +11,46 @@ end
 
 -- https://github.com/linrongbin16/gitlinker.nvim/blob/master/lua/gitlinker/range.lua#L14
 local function make_range()
-  local m = vim.fn.mode()
-  local l1 = nil
-  local l2 = nil
-  if is_visual_mode(m) then
-    vim.cmd([[execute "normal! \<ESC>"]])
-    l1 = vim.fn.getpos("'<")[2]
-    l2 = vim.fn.getpos("'>")[2]
-  else
-    l1 = vim.fn.getcurpos()[2]
-    l2 = l1
-  end
-  local lstart = math.min(l1, l2)
-  local lend = math.max(l1, l2)
-  local o = {
-    lstart = lstart,
-    lend = lend,
-  }
-  return o
+	local m = vim.fn.mode()
+	local l1 = nil
+	local l2 = nil
+	if is_visual_mode(m) then
+		vim.cmd([[execute "normal! \<ESC>"]])
+		l1 = vim.fn.getpos("'<")[2]
+		l2 = vim.fn.getpos("'>")[2]
+	else
+		l1 = vim.fn.getcurpos()[2]
+		l2 = l1
+	end
+	local lstart = math.min(l1, l2)
+	local lend = math.max(l1, l2)
+	local o = {
+		lstart = lstart,
+		lend = lend,
+	}
+	return o
 end
 
-local function get_commit()
+local function get_commit(range_table)
 	local query = [[
   (comment) @comment
 ]]
 	local captures = vim.treesitter.query.parse("lua", query)
 	local tree = vim.treesitter.get_parser():parse()[1]
-	for id, node, metadata in captures:iter_captures(tree:root(), 0) do
+	for id, node, metadata in captures:iter_captures(tree:root(), 0, range_table.lstart - 1, range_table.lend) do
 		local start_row, start_col, end_row, end_col = node:range()
-
-		-- local comment_text = vim.treesitter.get_node_text(node, vim.api.nvim_get_current_buf())
-		-- print(string.format("Line %d: %s", start_row + 1, comment_text))
+		start_row = start_row + 1
+		end_row = end_row + 1
 		print(start_row, start_col, end_row, end_col)
 	end
 end
 
 function eraser.init()
 	vim.api.nvim_create_user_command("EraseCommit", function()
-        local range =make_range()
-        lib.print(range)
+		local range = make_range()
+		-- lib.print(range)
+		print(range.lend, range.lstart)
+		get_commit(range)
 	end, {})
 	local map = vim.api.nvim_set_keymap
 	local opt = { noremap = true, silent = true }

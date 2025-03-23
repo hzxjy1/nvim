7e5b1f39a9396d2d604e39419971a981e77755e1
Hellagur4225 <Hellagur4225@outlook.com>
2025-03-23 15:47:08 +0800

    Temporary disable plugin neoscroll

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,8 +1,8 @@
 local config = {
-	"karb94/neoscroll.nvim",
-	config = function()
-		require("neoscroll").setup({})
-	end,
+	-- "karb94/neoscroll.nvim",
+	-- config = function()
+	-- 	require("neoscroll").setup({})
+	-- end,
 }
 
 return config

66340c5a70a34510b673965b28f7a8f39e1fa7b2
Hellagur4225 <Hellagur4225@outlook.com>
2025-04-09 20:32:11 +0800

    Too Lazy to install Nerd fonts, so I use emoji🤗

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,7 +1,17 @@
 local config = {
 	"folke/todo-comments.nvim",
 	config = function()
-		require("todo-comments").setup({})
+		require("todo-comments").setup({
+			keywords = {
+				FIX = { icon = "🔧", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+				TODO = { icon = "📝", color = "info" },
+				HACK = { icon = "⚡", color = "warning" },
+				WARN = { icon = "⚠️", color = "warning", alt = { "WARNING", "XXX" } },
+				PERF = { icon = "🚀", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+				NOTE = { icon = "ℹ️", color = "hint", alt = { "INFO" } },
+				TEST = { icon = "🧪", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+			},
+		})
 	end,
 }
 

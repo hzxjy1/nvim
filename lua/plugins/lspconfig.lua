7e72f333a89603ad745a43478d7ce40598d323a3
Hellagur4225 <Hellagur4225@outlook.com>
2025-04-23 13:51:24 +0800

    Chore: fix typo

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,7 @@
 local config = {
 	"neovim/nvim-lspconfig",
 	config = function()
-		local lang_conf = lib.module_loader("trinity") -- HACK: util.get_conf need inport
+		local lang_conf = lib.module_loader("trinity") -- HACK: util.get_conf need import
 		local lspconfig = require("lspconfig")
 		fp.map(lang_conf, function(obj)
 			if obj.lsp ~= nil and not lib.is_include(conf.disabled_lsp, obj.name) then

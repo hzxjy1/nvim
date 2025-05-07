c15ad8fa25d59e37fd74dac92d6138e4747c26b7
Hellagur4225 <Hellagur4225@outlook.com>
2025-05-07 16:01:34 +0800

    Refactor: optimize fliter func in mason-lspconfig

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -11,28 +11,23 @@ local function get_install_list()
 	end)))
 end
 
+local function executable_check(exec_list)
+	return function(ensure_installed)
+		return fp.filter(ensure_installed, function(item)
+			return not (vim.tbl_contains(exec_list, item) and vim.fn.executable(item) == 1)
+		end)
+	end
+end
+
 local mason_lspconfig_setup = {
-	ensure_installed = get_install_list(),
+	ensure_installed = executable_check({ "clangd" })(get_install_list()),
 	automatic_installation = true,
 }
-
-function mason_lspconfig_setup:clangd_check()
-	local lsp = "clangd"
-	if lib.is_executable(lsp) then
-		for i, item in ipairs(self.ensure_installed) do
-			if item == lsp then
-				table.remove(self.ensure_installed, i)
-				break
-			end
-		end
-	end
-	return self
-end
 
 local config = {
 	"williamboman/mason-lspconfig.nvim",
 	config = function()
-		require("mason-lspconfig").setup(mason_lspconfig_setup:clangd_check())
+		require("mason-lspconfig").setup(mason_lspconfig_setup)
 	end,
 }
 

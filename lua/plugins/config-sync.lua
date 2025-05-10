8980e5896ccef18be470bec006468fce6f228e02
Hellagur4225 <Hellagur4225@outlook.com>
2025-05-10 12:47:24 +0800

    Chore: update conf of config-sync

--- lua/plugins/config-sync.lua
+++ lua/plugins/config-sync.lua
@@ -1,9 +1,9 @@
 local config = {
-    "hzxjy1/config-sync.nvim",
-    -- dir = "~/project/sub/config-sync.nvim",
-    config = function()
-        require("config-sync").setup({})
-    end
+	"hzxjy1/config-sync.nvim",
+	-- dir = "~/project/sub/config-sync.nvim",
+	config = function()
+		require("config-sync").setup({ target_branch = "master" })
+	end,
 }
 
 return config

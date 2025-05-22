33774ea6804c17ec5144e61b22f8e3372d4d8107
Hellagur4225 <Hellagur4225@outlook.com>
2025-05-22 16:33:43 +0800

    Fix: add nil detection in lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,6 +3,9 @@ local config = {
 	config = function()
 		local lang_conf = lib.module_loader("trinity") -- HACK: util.get_conf need import
 		local lspconfig = require("lspconfig")
+		if conf.disabled_lsp == nil then
+			conf.disabled_lsp = {}
+		end
 		fp.map(lang_conf, function(obj)
 			if obj.lsp ~= nil and not vim.tbl_contains(conf.disabled_lsp, obj.name) then
 				lspconfig[obj.lsp].setup(obj.lsp_setup(lspconfig))

79b2918c2cc336ce40e7f0f032ac5a78a4b3df50
Hellagur4225 <Hellagur4225@outlook.com>
2025-07-16 17:30:14 +0800

    Feat: Automatically turn on inlay_hint

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,3 +1,22 @@
+local function on_attach_hook(lspconfig)
+	return function(lang_obj)
+		local setup = lang_obj.lsp_setup(lspconfig)
+		local original_on_attach = setup.on_attach
+
+		setup.on_attach = function(client, bufnr)
+			if original_on_attach then
+				original_on_attach(client, bufnr)
+			end
+
+			if client.supports_method("textDocument/inlayHint") then
+				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+			end
+		end
+
+		return setup
+	end
+end
+
 local config = {
 	"neovim/nvim-lspconfig",
 	config = function()
@@ -8,7 +27,7 @@ local config = {
 		end
 		fp.map(lang_conf, function(obj)
 			if obj.lsp ~= nil and not vim.tbl_contains(conf.disabled_lsp, obj.name) then
-				lspconfig[obj.lsp].setup(obj.lsp_setup(lspconfig))
+				lspconfig[obj.lsp].setup(on_attach_hook(lspconfig)(obj))
 			end
 		end)
 	end,
